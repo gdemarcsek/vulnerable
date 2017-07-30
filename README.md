7679d22479e56efe5e8339a6cfa93745c7d5d46d
György Demarcsek <gyorgy.demarcsek@prezi.com>
2017-07-30 13:37:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Some vulnerable Python service to demonstrate some generic and ffmpeg-specific vulns and how AppArmor can help to immunize our user facing apps.
---
+++ app.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+import os
+import time
+from hashlib import sha1
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+import subprocess
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['RESULT_FOLDER'] = 'converted/'
+app.config['ALLOWED_EXTENSIONS'] = set(['mp4', 'avi', 'vmw', 'mkv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    scale_param = request.form['scale']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        input_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+	output_file_path = os.path.join(app.config['RESULT_FOLDER'], sha1("%s_%s" % (filename, time.time())).hexdigest())
+	file.save(input_file_path)
+	# we will also do this with a vulnerable ffmpeg version...
+        ffmpeg = subprocess.Popen(["ffmpeg", "-i", input_file_path, "-vf", "scale=%s" % (scale_param, output_file_path)], shell=True)
+	ffmpeg_resut = ffmpeg.wait()
+	if ffmpeg_result == 0:
+		return redirect(url_for('uploaded_file', filename=os.path.basename(output_file_path)))
+	else:
+		return "Conversion failed", 400
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['RESULT_FOLDER'],
+                               filename)
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=int("8080"),
+        debug=True
+    )
+
