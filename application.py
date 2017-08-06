c0ce6f6306c7dc53f05aa121e6c422222be49625
György Demarcsek <gyorgy.demarcsek@prezi.com>
2017-08-06 13:35:00 +0200

    some cleanups

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
-import sys
 import os
 import time
-import ftplib
 from hashlib import sha1
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
@@ -15,26 +13,31 @@ app.config['UPLOAD_FOLDER'] = base_dir + '/uploads/'
 app.config['RESULT_FOLDER'] = base_dir + '/converted/'
 app.config['ALLOWED_EXTENSIONS'] = set(['mp4', 'avi', 'vmw', 'mkv'])
 
+
 def load_config():
     app.config.from_pyfile('config.cfg')
+
 
 @app.before_first_request
 def app_setup():
     aa = aalight.apparmor()
     load_config()
     try:
-    	aa.change_hat("SERVING")
+        aa.change_hat("SERVING")
     except OSError as error:
-		if os.getenv("ENVIRONMENT", "production") != "production":
-			raise error
+        if os.getenv("ENVIRONMENT", "production") != "production":
+            raise error
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -46,17 +49,19 @@ def upload():
         # Make the filename safe, remove unsupported chars
         filename = secure_filename(file.filename)
         input_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-	output_file_path = os.path.join(app.config['RESULT_FOLDER'], sha1("%s_%s" % (filename, time.time())).hexdigest())
-	file.save(input_file_path)
-	# we will also do this with a vulnerable ffmpeg version...
-	output_file_path += ".mp4"
-        command = ["ffmpeg", "-i", input_file_path, "-vf", "scale=%s" % scale_param, "-strict", "-2", "-y", output_file_path]
-	print(" ".join(command))
-	ffmpeg_result = subprocess.call(command)
-	if ffmpeg_result == 0:
-		return redirect(url_for('uploaded_file', filename=os.path.basename(output_file_path)))
-	else:
-		return "Conversion failed", 400
+        output_file_path = os.path.join(app.config['RESULT_FOLDER'],
+                                        sha1("%s_%s" % (filename, time.time())).hexdigest())
+        file.save(input_file_path)
+        # we will also do this with a vulnerable ffmpeg version...
+        output_file_path += ".mp4"
+        command = ["ffmpeg", "-i", input_file_path, "-vf", "scale=%s" % scale_param, "-strict", "-2", "-y",
+                   output_file_path]
+        ffmpeg_result = subprocess.call(command)
+        if ffmpeg_result == 0:
+            return redirect(url_for('uploaded_file', filename=os.path.basename(output_file_path)))
+        else:
+            return "Conversion failed", 400
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
