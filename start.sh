96f273153f01a9531d17feaa9c7ac1be3e92e5fc
György Demarcsek <gyorgy.demarcsek@prezi.com>
2017-09-03 15:46:00 +0200

    start.sh: fix pip install

--- start.sh
+++ start.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 CWD="$(dirname $0)"
 
 if [ ! -d "$CWD/virtualenv" ]; then
-    cd $CWD && virtualenv virtualenv && pip install -r requirements.txt
+    cd "$CWD" && virtualenv virtualenv
 fi
 
 source "$CWD/virtualenv/bin/activate"
+
+pip install -r requirements.txt
 
 cd "$CWD" && gunicorn -c gunicorn.conf.py wsgi
 
